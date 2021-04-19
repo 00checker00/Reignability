abaa4eba9256aee12e03c490147b5a4f1857dace
Laptop <antonkrapp@msn.com>
2021-04-19 13:49:59 +0200

    Leveloader Anfang

---
+++ app/choose.ts
@@ -0,0 +1,16 @@
+import { LevelLoader } from "./levelLoader";
+
+export class Choose {
+
+    private lvlLoad: LevelLoader;
+
+    
+    constructor(loader: LevelLoader) {
+        
+        this.lvlLoad = loader;
+
+    }
+
+
+    
+}
